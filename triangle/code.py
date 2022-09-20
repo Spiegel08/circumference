a35d79a80d8dfac0a928fe0cd1aed77ae9fe33ea
Spiegel08 <108896685+Spiegel08@users.noreply.github.com>
2022-09-20 21:13:48 +0300

    Create code.py

---
+++ triangle/code.py
@@ -0,0 +1,9 @@
+a, b, c = 10, 10, 10
+
+
+def triangle_perimeter(side_a = a, side_b = b, side_c = c):
+  return side_a + side_b + side_b
+
+def triangle_area(side_a = a, side_b = b, side_c = c):
+  pass
+  
