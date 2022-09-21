1c8e9c905db941905f6c5294f50413baca7f7a91
Spiegel08 <108896685+Spiegel08@users.noreply.github.com>
2022-09-21 09:29:58 +0300

    Update code.py

--- triangle/code.py
+++ triangle/code.py
@@ -1,7 +1,7 @@
 a, b, c = 10, 10, 10
 
 def triangle_perimeter(side_a = a, side_b = b, side_c = c):
-  return side_a + side_b + side_b
+  return side_a + side_b + side_c
 
 def triangle_area(side_a = a, side_b = b, side_c = c):
   p = (side_a + side_b + side_c) / 2
